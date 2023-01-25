b110a4cd652d07299291c5d81810c76e9bade652
Chandu <gangadhara-rao.chandu@capgemini.com>
2023-01-25 14:42:44 +0530

    Constructor

--- javabasic/javabasic/javaopps/src/Methodoverriding/Animal.java
+++
@@ -1,9 +0,0 @@
-package Methodoverriding;
-
-public class Animal {
-
-		  public void animalSound() {
-		    System.out.println("The animal makes a sound");
-		  }
-		
-}
--- javabasic/javabasic/javaopps/src/Methodoverriding/Cat.java
+++
@@ -1,9 +0,0 @@
-package Methodoverriding;
-
-public class Cat extends Animal {
-	
-		  public void animalSound() {
-		    System.out.println("The cat says: meowee meowwee");
-		  }
-		
-}
